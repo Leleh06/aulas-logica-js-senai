ebf9fe3b98b5045eec6900ded15ab01db86e985e
LETICYA SILVA DIAS <leticya_dias@estudante.sesisenai.org.br>
2025-04-16 22:18:30 -0300

    Inicio do JS

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+// declaração de variável com var
+
+/*var nome = "Leticya"
+
+var nome = "Dias"*/
+
+// declaração de variável com let 
+
+// let nome = "Leticya"
+
+// nome = "Dias"
+
+// variavel declarada com cons
+
+const nome = "Leticya"
+
+nome = "Dias"
+
+console.log(nome)
